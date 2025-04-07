2b56a6602ed9226d333e09bd5f984bffc863899d
janelle <145067236+jan5ll5@users.noreply.github.com>
2025-04-07 11:46:45 -0700

    faalling bblock
    did falling block and now doing color

--- StackSurgeP3/Assets/MovingCube.cs
+++ StackSurgeP3/Assets/MovingCube.cs
@@ -8,7 +8,7 @@ public class MovingCube : MonoBehaviour
     public static MovingCube LastCube {  get; private set; }
 
     [SerializeField]
-    private float moveSpeed = 2f;
+    private float moveSpeed = 4f;
 
     private void OnEnable()
     {
@@ -18,6 +18,12 @@ public class MovingCube : MonoBehaviour
         }
 
         CurrentCube = this;
+        GetComponent<Renderer>().material.color = GetRandomColor();
+    }
+
+    private void GetRandomColor()
+    {
+        return new Color(UnityEngine.Random.Range(0, 1f), UnityEngine.Random.Range(0, 1f), UnityEngine.Random.Range(0, 1f));
     }
 
     internal void Stop()
@@ -26,10 +32,11 @@ public class MovingCube : MonoBehaviour
         float hangover = transform.position.z - LastCube.transform.position.z;
         Debug.Log(hangover);
 
-        SplitCubeOnZ(hangover);
+        float direction = hangover > 0 ? 1f : -1f;
+        SplitCubeOnZ(hangover, direction);
     }
 
-    private void SplitCubeOnZ(float hangover)
+    private void SplitCubeOnZ(float hangover, float direction)
     {
         float newZSize = LastCube.transform.localScale.z - Mathf.Abs(hangover);
         float fallingBlockSize = transform.localScale.z - newZSize;
@@ -38,8 +45,21 @@ public class MovingCube : MonoBehaviour
         transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, newZSize);
         transform.position = new Vector3(transform.position.x, transform.position.y, newZPosition);
 
-        float cubeEdge = transform.position.z + (newZSize / 2f);
-        float fallingBlockZPosition = cubeEdge + fallingBlockSize / 2f;
+        float cubeEdge = transform.position.z + (newZSize / 2f * direction);
+        float fallingBlockZPosition = cubeEdge + fallingBlockSize / 2f * direction;
+
+        
+        SpawnDropCube(fallingBlockZPosition, fallingBlockSize);
+    }
+
+    private void SpawnDropCube(float fallingBlockZPosition, float fallingBlockSize)
+    {
+        var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, fallingBlockSize);
+        cube.transform.position = new Vector3(transform.position.x, transform.position.y, fallingBlockZPosition);
+
+        cube.AddComponent<Rigidbody>();
+        Destroy(cube.gameObject, 3f);
     }
 
     // Update is called once per frame
